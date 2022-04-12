e41dccd7ec804348b504f2ed6597b48bec95e57c
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2022-04-12 19:40:44 +0200

    completed programming quiz

--- main.c
+++ main.c
@@ -15,7 +15,187 @@ void print_menu(){
     printf("\n+-------------------------------------+");
 }
 void programming_quiz(){
+    int prog_score = 0;
+    char prog_ans;
+
     printf("Programming Quiz\n");
+    printf("Choose your answer by typing the appropriate letter\n");
+
+    //1.
+    printf("\n1. How to display output in Java?\n");
+    printf("a) print()\n");
+    printf("b) System.out.println()\n");
+    printf("c) printf()\n");
+    printf("d) cout>>\n");
+    printf("Your answer: \n");
+    scanf("%s", &prog_ans);
+
+    if(prog_ans == 'b'){
+        printf("Correct!\n");
+        prog_score += 10;
+    }
+    else{
+        printf("Your answer: %c\nCorrect answer: b) System.out.println()\n", prog_ans);
+    }
+
+    //2.
+    printf("\n2. Which programming language is the predecessor of C?\n");
+    printf("a) C++\n");
+    printf("b) B++\n");
+    printf("c) B\n");
+    printf("d) Mini C\n");
+    printf("Your answer: \n");
+    scanf("%s", &prog_ans);
+
+    if(prog_ans == 'c'){
+        printf("Correct!\n");
+        prog_score += 10;
+    }
+    else{
+        printf("Your answer: %c\nCorrect answer: c) B\n", prog_ans);
+    }
+
+    //3.
+    printf("\n3. Which of the following is not a programming language?\n");
+    printf("a) TypeScript\n");
+    printf("b) Python\n");
+    printf("c) Anaconda\n");
+    printf("d) Java\n");
+    printf("Your answer: \n");
+    scanf("%s", &prog_ans);
+
+    if(prog_ans == 'c'){
+        printf("Correct!\n");
+        prog_score += 10;
+    }
+    else{
+        printf("Your answer: %c\nCorrect answer: c) Anaconda\n", prog_ans);
+    }
+
+    //4.
+    printf("\n4. What word is missing from the SQL line below?\n");
+    printf("SELECT *\n______ CUSTOMER\n");
+    printf("a) FROM\n");
+    printf("b) SELECT\n");
+    printf("c) WHERE\n");
+    printf("d) ORDER BY\n");
+    printf("Your answer: \n");
+    scanf("%s", &prog_ans);
+
+    if(prog_ans == 'a'){
+        printf("Correct!\n");
+        prog_score += 10;
+    }
+    else{
+        printf("Your answer: %c\nCorrect answer: a) FROM\n", prog_ans);
+    }
+
+    //5.
+    printf("\n5. Python is a ______ programming language.\n");
+    printf("a) low-level\n");
+    printf("b) mid-level\n");
+    printf("c) high-level\n");
+    printf("d) none of the above\n");
+    printf("Your answer: \n");
+    scanf("%s", &prog_ans);
+
+    if(prog_ans == 'c'){
+        printf("Correct!\n");
+        prog_score += 10;
+    }
+    else{
+        printf("Your answer: %c\nCorrect answer: c) high-level\n", prog_ans);
+    }
+
+    //6.
+    printf("\n6. Which command will stop an infinite loop?\n");
+    printf("a) Alt + C\n");
+    printf("b) Shift + C\n");
+    printf("c) Esc\n");
+    printf("d) Ctrl + C\n");
+    printf("Your answer: \n");
+    scanf("%s", &prog_ans);
+
+    if(prog_ans == 'd'){
+        printf("Correct!\n");
+        prog_score += 10;
+    }
+    else{
+        printf("Your answer: %c\nCorrect answer: d) Ctrl + C\n", prog_ans);
+    }
+
+    //7.
+    printf("\n7. Which of the following is the correct way of making a string in Java?\n");
+    printf("a) String \"Text\";\n");
+    printf("b) String text = 'text';\n");
+    printf("c) String text = \"text\";\n");
+    printf("d) String text = \"text\";\n");
+    printf("Your answer: \n");
+    scanf("%s", &prog_ans);
+
+    if(prog_ans == 'd'){
+        printf("Correct!\n");
+        prog_score += 10;
+    }
+    else{
+        printf("Your answer: %c\nCorrect answer: d) String text = \"text\";\n", prog_ans);
+    }
+
+    //8.
+    printf("\n8. Which of the following is a valid define statement?\n");
+    printf("a) #define MAX=200\n");
+    printf("b) #define MAX 200\n");
+    printf("c) # define MAX 200\n");
+    printf("d) #define MAX 200;\n");
+    printf("Your answer: \n");
+    scanf("%s", &prog_ans);
+
+    if(prog_ans == 'b'){
+        printf("Correct!\n");
+        prog_score += 10;
+    }
+    else{
+        printf("Your answer: %c\nCorrect answer: b) #define MAX 200\n", prog_ans);
+    }
+
+    //9.
+    printf("\n9. How many keywords are recognized by standard ANSI C?\n");
+    printf("a) 30\n");
+    printf("b) 24\n");
+    printf("c) 32\n");
+    printf("d) 36\n");
+    printf("Your answer: \n");
+    scanf("%s", &prog_ans);
+
+    if(prog_ans == 'c'){
+        printf("Correct!\n");
+        prog_score += 10;
+    }
+    else{
+        printf("Your answer: %c\nCorrect answer: c) 32\n", prog_ans);
+    }
+
+    //10.
+    printf("\n10. What is the process of finding and fixing errors within a program called?\n");
+    printf("a) Debugging\n");
+    printf("b) Compiling\n");
+    printf("c) Executing\n");
+    printf("d) Scanning\n");
+    printf("Your answer: \n");
+    scanf("%s", &prog_ans);
+
+    if(prog_ans == 'a'){
+        printf("Correct!\n");
+        prog_score += 10;
+    }
+    else{
+        printf("Your answer: %c\nCorrect answer: a) Debugging\n", prog_ans);
+    }
+
+    //score
+    int prog_correct = prog_score / 10;
+    printf("\nYour score: %d / 100\n", prog_score);
+    printf("%d / 10 correct\n", prog_correct);
 }
 
 void computer_science_quiz(){
@@ -31,8 +211,7 @@ void physics_quiz(){
 }
 
 int main(void) {
-    int choice;
-    int loop = 1;
+    int choice, loop = 1;
     print_menu();
 
     while (loop) {
@@ -44,7 +223,7 @@ int main(void) {
                 printf("Programming Quiz selected\n");
                 while (1){
                     char option;
-                    printf("Press 'C' to continue or press 'E' to exit: \n");
+                    printf("\nPress 'C' to continue or press 'E' to exit: \n");
                     scanf(" %c", &option);
 
                     if(option == 'C'){
@@ -52,7 +231,7 @@ int main(void) {
                         break;
                     }
                     else if(option == 'E'){
-                        printf("Programming Quiz closed\n");
+                        printf("\nProgramming Quiz closed\n");
                         print_menu();
                         break;
                     }
@@ -63,7 +242,7 @@ int main(void) {
                 printf("Computer Science Quiz selected\n");
                 while(1){
                     char option;
-                    printf("Press 'C' to continue or press 'E' to exit: \n");
+                    printf("\nPress 'C' to continue or press 'E' to exit: \n");
                     scanf(" %c", &option);
 
                     if(option == 'C'){
@@ -71,7 +250,7 @@ int main(void) {
                         break;
                     }
                     else if(option == 'E'){
-                        printf("Computer Science Quiz closed\n");
+                        printf("\nComputer Science Quiz closed\n");
                         print_menu();
                         break;
                     }
@@ -82,7 +261,7 @@ int main(void) {
                 printf("Mathematics Quiz selected\n");
                 while(1){
                     char option;
-                    printf("Press 'C' to continue or press 'E' to exit: \n");
+                    printf("\nPress 'C' to continue or press 'E' to exit: \n");
                     scanf(" %c", &option);
 
                     if(option == 'C'){
@@ -90,7 +269,7 @@ int main(void) {
                         break;
                     }
                     else if(option == 'E'){
-                        printf("Mathematics Quiz closed\n");
+                        printf("\nMathematics Quiz closed\n");
                         print_menu();
                         break;
                     }
@@ -101,7 +280,7 @@ int main(void) {
                 printf("Physics Quiz selected\n");
                 while(1){
                     char option;
-                    printf("Press 'C' to continue or press 'E' to exit: \n");
+                    printf("\nPress 'C' to continue or press 'E' to exit: \n");
                     scanf(" %c", &option);
 
                     if(option == 'C'){
@@ -109,7 +288,7 @@ int main(void) {
                         break;
                     }
                     else if(option == 'E'){
-                        printf("Physics Quiz closed\n");
+                        printf("\nPhysics Quiz closed\n");
                         print_menu();
                         break;
                     }
